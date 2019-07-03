ce5489de29e0156873f4d18149b6f44a984df930
z7005881 <z7005881@LAPTOP-IHG1UFUH>
2019-07-03 14:22:26 +0800

    test6

--- src/com/neusoft/system/db/DBUtils.java
+++ src/com/neusoft/system/db/DBUtils.java
@@ -30,6 +30,16 @@ public class DBUtils
 		{
 			System.out.println("测试github更改333333333333");
 			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
+			System.out.println("测试github更改333333333333");
 			//获取资源文件解析器对象
 			ResourceBundle bundle=ResourceBundle.getBundle("DBOptions");
 			//通过解析对象,获取数据
